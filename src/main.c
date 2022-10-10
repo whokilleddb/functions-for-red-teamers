ab0de13c5ed49fd3a5108fec60bde4fc6bb7b3a2
whokilleddb <whokilleddb@protonmail.com>
2022-10-11 04:35:42 +0530

    Updated main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 #include "rewrite.h"
 
 int main(void){
     char func[] = "AcquireSRWLockExclusive";
     HMODULE _kernel32 = LoadLibrary((LPCWSTR)"kernel32.dll");
 
-    assert(__get_proc_address(_kernel32, "AcquireSRWLockExclusive"), GetProcAddress(_kernel32, "AcquireSRWLockExclusive"));
+    assert(__get_proc_address(_kernel32, "AcquireSRWLockExclusive") == GetProcAddress(_kernel32, "AcquireSRWLockExclusive"));
     
     return 0;
 }
